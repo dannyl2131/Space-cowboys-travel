febed703f7f21ab2571b3297d7ee65fa00fafab0
bronson <disnamedoe@gmail.com>
2023-04-03 21:06:23 -0700

    entire class day

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
-const stars= document.querySelectorAll('.stars a');
-stars.forEach((star, idx) => { 
-    star.addEventListener('click', ()=>{
-        console.log('star of index ${idx} was clicked');
-        //
-    });
-});
+const star= $("#star")
+
+star.click(function(){
+    console.log(query.val())
+})
 
 let favorites = $("#favorites")
 let forecast = $("#forecast")
@@ -33,9 +31,10 @@ let weatherSearch = function(param){
             let div4 = document.createElement("div")
             let div5 = document.createElement("div")
             let div6 = document.createElement("div")
-            let h1 = document.createElement("h1")
-            let h2 = document.createElement("h2")
-           forecast.append(div1)
+            let icon = document.createElement("i")
+            let temperature = document.createElement("h2")
+            let wind = document.createElement("h2")
+            forecast.append(div1)
             div1.classList.add("column")
             div2.classList.add("ui")
             div2.classList.add("card")
@@ -47,11 +46,14 @@ let weatherSearch = function(param){
             div3.append(div4)
             div3.append(div5)
             div3.append(div6)
-            div6.append(h1)
-            div6.append(h2)  
-          
-           
+            div6.append(icon)
+            div6.append(temperature)
+            div6.append(wind)
+            temperature.innerHTML = "Temperature: " + item.main.temp + "F"
+            wind.innerHTML = "Wind speed: "  + item.wind.speed + "MPH"
+            icon.innerHTML = "<img src=https://openweathermap.org/img/wn/" + item.weather[0].icon + "@2x.png>"
         })
+        
     })
 
 }
@@ -63,7 +65,7 @@ let weatherSearch = function(param){
 var cityfetch = "https://app.ticketmaster.com/discovery/v2/events.json?&dmaId=324&apikey=IsHdraDgQ6AybSMTXCgWdC4WQFWUkgBn"
 
 const allcities = [
-    { number: 212, name: 'abilene - sweetwater' },
+    { number: 212, name: 'abilene' },
   { number: 213, name: 'albany' },
   { number: 214, name: 'albany, GA' },
   { number: 215, name: 'albuquerque' },
@@ -148,7 +150,128 @@ const allcities = [
 { number: 298, name: 'helena' },
 { number: 299, name: 'honolulu' },
 { number: 300, name: 'houston' },
-{ number: 301, name: 'huntsville' }
+{ number: 301, name: 'huntsville' },
+{ number: 302, name: 'idaho falls' },
+{ number: 303, name: 'indianapolis' },
+{ number: 304, name: 'jackson, ms' },
+{ number: 305, name: 'jackson, tn' },
+{ number: 306, name: 'jacksonville' },
+{ number: 307, name: 'johnstown' },
+{ number: 308, name: 'jonesboro' },
+{ number: 309, name: 'joplin' },
+{ number: 310, name: 'juneau' },
+{ number: 311, name: 'kansas city' },
+{ number: 312, name: 'knoxville' },
+{ number: 313, name: 'la crosse' },
+{ number: 314, name: 'lafayette, in' },
+{ number: 315, name: 'lafayette, la' },
+{ number: 316, name: 'lake charles' },
+{ number: 317, name: 'lansing' },
+{ number: 318, name: 'laredo' },
+{ number: 319, name: 'las vegas' },
+{ number: 320, name: 'lexington' },
+{ number: 321, name: 'lima' },
+{ number: 322, name: 'lincoln' },
+{ number: 323, name: 'little rock' },
+{ number: 324, name: 'los angeles' },
+{ number: 325, name: 'louisville' },
+{ number: 326, name: 'lubbock' },
+{ number: 327, name: 'macon' },
+{ number: 328, name: 'madison' },
+{ number: 329, name: 'mankato' },
+{ number: 330, name: 'marquette' },
+{ number: 331, name: 'medford' },
+{ number: 332, name: 'memphis' },
+{ number: 333, name: 'meridian' },
+{ number: 334, name: 'miami' },
+{ number: 335, name: 'milwaukee' },
+{ number: 336, name: 'minneapolis' },
+{ number: 337, name: 'minot' },
+{ number: 338, name: 'missoula' },
+{ number: 339, name: 'mobile' },
+{ number: 340, name: 'monroe' },
+{ number: 341, name: 'monterey' },
+{ number: 342, name: 'montgomery' },
+{ number: 343, name: 'nashville' },
+{ number: 344, name: 'new orleans' },
+{ number: 345, name: 'new york' },
+{ number: 346, name: 'norfolk' },
+{ number: 347, name: 'north platte' },
+{ number: 348, name: 'odessa' },
+{ number: 349, name: 'oklahoma' },
+{ number: 350, name: 'omaha' },
+{ number: 351, name: 'orlando'},
+{ number: 352, name: 'ottumwa' },
+{ number: 353, name: 'paducah' },
+{ number: 354, name: 'palm springs' },
+{ number: 355, name: 'panama city' },
+{ number: 356, name: 'parkersburg' },
+{ number: 357, name: 'peoria' },
+{ number: 358, name: 'philadelphia' },
+{ number: 359, name: 'phoenix' },
+{ number: 360, name: 'pittsburgh' },
+{ number: 361, name: 'portland  auburn' },
+{ number: 362, name: 'portland' },
+{ number: 363, name: 'presque isle' },
+{ number: 364, name: 'providence' },
+{ number: 365, name: 'quincy' },
+{ number: 366, name: 'raleigh' },
+{ number: 367, name: 'rapid city' },
+{ number: 368, name: 'reno' },
+{ number: 369, name: 'richmond' },
+{ number: 370, name: 'roanoke' },
+{ number: 371, name: 'rochester' },
+{ number: 373, name: 'rockford' },
+{ number: 374, name: 'sacramento' },
+{ number: 375, name: 'saint joseph' },
+{ number: 376, name: 'saint louis' },
+{ number: 377, name: 'salisbury' },
+{ number: 378, name: 'salt lake city' },
+{ number: 379, name: 'san angelo' },
+{ number: 380, name: 'san antonio' },
+{ number: 381, name: 'san diego' },
+{ number: 382, name: 'san francisco' },
+{ number: 383, name: 'santa barbara' },
+{ number: 384, name: 'savannah' },
+{ number: 385, name: 'seattle' },
+{ number: 386, name: 'sherman' },
+{ number: 387, name: 'shreveport' },
+{ number: 388, name: 'sioux city' },
+{ number: 389, name: 'sioux falls' },
+{ number: 390, name: 'south bend' },
+{ number: 391, name: 'spokane' },
+{ number: 392, name: 'springfield' },
+{ number: 393, name: 'springfield' },
+{ number: 394, name: 'syracuse' },
+{ number: 395, name: 'tallahassee' },
+{ number: 396, name: 'tampa' },
+{ number: 397, name: 'terre haute' },
+{ number: 398, name: 'toledo' },
+{ number: 399, name: 'topeka'},
+{ number: 400, name: 'traverse city' },
+{ number: 401, name: 'tri-cities' },
+{ number: 402, name: 'tucson' },
+{ number: 403, name: 'tulsa' },
+{ number: 404, name: 'twin falls' },
+{ number: 405, name: 'tyler' },
+{ number: 406, name: 'utica' },
+{ number: 407, name: 'victoria' },
+{ number: 408, name: 'waco ' },
+{ number: 409, name: 'washington dc' },
+{ number: 410, name: 'watertown' },
+{ number: 411, name: 'wausau' },
+{ number: 412, name: 'west palm beach' },
+{ number: 413, name: 'wheeling' },
+{ number: 414, name: 'wichita' },
+{ number: 415, name: 'wichita falls' },
+{ number: 416, name: 'wilkes barre' },
+{ number: 417, name: 'wilmington' },
+{ number: 418, name: 'yakima' },
+{ number: 419, name: 'youngstown' },
+{ number: 420, name: 'yuma' },
+{ number: 421, name: 'zanesville' },
+{ number: 422, name: 'northern new jersey' },
+
 ]
 
 
@@ -162,17 +285,29 @@ $('#searchButton').click(function(event) {
             console.log("i work");
             console.log(allcities[i]);
             citynum = allcities[i].number;
-            fetch("https://app.ticketmaster.com/discovery/v2/events.json?&dmaId=" + 
-            citynum + "&apikey=IsHdraDgQ6AybSMTXCgWdC4WQFWUkgBn")
+            fetch("https://app.ticketmaster.com/discovery/v2/events.json?&dmaId=" + citynum + "&apikey=IsHdraDgQ6AybSMTXCgWdC4WQFWUkgBn")
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function(data) {
                     console.log(data._embedded.events);
-                    for (var i = 0; i < 6; i++) {
-                        console.log(data._embedded.events[i].name);
-                    }
+                    var tempevent = [];
+                    for (var i = 0; i < 10; i++) {
+                        if (i === 0) {
+                            tempevent.unshift(data._embedded.events[i].name);
+                        }
+                        else if (data._embedded.events[i].name === data._embedded.events[i-1].name) {
+                            data._embedded.events.splice(i, 1);
+                            i--;
+                        } else {
+                            tempevent.unshift(data._embedded.events[i].name);
+                        }
+                    } console.log(tempevent);
+
                 })
+        } else {
+            console.log("not a valid city");
         }
     }
 })
+
